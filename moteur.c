a202bc5ce7724fef9410300a27c8df825ff877da
loulou54univlorraine <louis.dudot7@etu.univ-lorraine.fr>
2016-10-14 09:56:48 +0200

    re

--- moteur.c
+++ moteur.c
@@ -5,27 +5,120 @@
 #include <math.h>
 #include <SDL/SDL.h>
 #include "moteur.h"
-#include "calcul.h"
 #include "sdl.h"
 
 #define VITESSE_DEPLACEMENT 5   
 #define VITESSE_ROTATION 3  
 
 
-extern int carte[LARGEUR_CARTE][HAUTEUR_CARTE]; 
 
-Camera camera1= {10,12,-1,0,0,0.60};
-Camera *pointeur = &camera1;
- 
+
+extern int carte[LARGEUR_CARTE][HAUTEUR_CARTE]; 
+/* Structure de couleur RGB */
+struct couleur {
+  int rouge;
+  int vert;
+  int bleu;
+};
+
+ /* Constantes couleur */
+const  Couleur noir    = {  0,  0,  0};
+const  Couleur rouge   = {255,  0,  0};
+const  Couleur vert    = {  0,255,  0};
+const  Couleur bleu    = {  0,  0,255};
+const  Couleur blanc   = {255,255,255};
+const  Couleur jaune   = {255,255,  0};
+const  Couleur cyan    = {  0,255,255};
+const  Couleur magenta = {255,  0,255};
+const  Couleur gris    = {128,128,128};
+const  Couleur marron  = {128,  0,  0};
+const  Couleur violet  = {128,  0,128};
+
+Couleur moteur_demi_couleur(Couleur ancienne) {
+  Couleur nouvelle;
+  nouvelle.rouge = ancienne.rouge / 2;
+  nouvelle.vert  = ancienne.vert  / 2;
+  nouvelle.bleu  = ancienne.bleu  / 2;
+  return nouvelle;
+};
+
+struct coordoneReel
+{
+  double posX;
+  double posY;
+};
+
+coordoneReel->posX = 12;
+coordoneReel->posY = 10;
+
+struct directionsRayon
+{
+  double dirX;
+  double dirY;
+};
+
+struct planCamera
+{
+  double planX;
+  double planY;
+};
+
+struct camera
+{
+  double cameraX;
+};
+
+
+struct positionRayon
+{
+  double rayPosX;
+  double rayPosy;
+};
+
+struct directionRayon
+{
+  double rayDirX;
+  double rayDirY;
+};
+
+struct positionCarte
+{
+int carteX;
+int carteY;
+};
+
+struct distanceBord
+{
+
+double bordDistX;
+double bordDistY;
+  
+};
+
+struct delta
+{
+  double deltaDistX ;
+  double deltaDistY ;
+};
+
+double perpWallDist;  /* Distance projetee sur la direction de la camera */
+
 /*---------------  raycasting     ---------------*/
 
 void moteur_dessiner(int x, int largeur, int hauteur) {
-pointeur->posX = 13;
- printf("%.3d w1 \n",(int)camera1.posX);
+
+
+
+ /* Couleur de la partie de mur a dessiner */
+
+  /* On calcule la position et la direction du rayon */
+ 
 
 }
 
 void moteur_gestion_des_actions(double temps_frame) {
+
+
 
   sdl_touches_lire();
   
--- moteur.h
+++ moteur.h
@@ -1,11 +1,33 @@
 #define LARGEUR_CARTE 24
 #define HAUTEUR_CARTE 24
 
-typedef struct Coordonnees Coordonnees;
+typedef struct PosJoueur PosJoueur;
 
-struct Coordonnees{
+struct PosJoueur{
 double posX; // Abscisses
 double posY; // Ordonnées
+};
+
+typedef struct DirectionJoueur DirectionJoueur;
+
+struct DirectionJoueur{
+double dirX; 
+double diry; 
+};
+
+
+typedef struct PlanCamera PlanCamera;
+
+struct PlanCamera{
+double planX;
+double planY;
+};
+
+typedef struct PosCarteJoueur PosCarteJoueur;
+
+struct PosCarteJoueur{
+int carteX;          /* Position du carre dans lequel on est en x */
+int carteY; 
 };
 
 
@@ -18,7 +40,42 @@ double dirX;
 double diry; 
 double planX;
 double planY;
+int carteX;          /* Position du carre dans lequel on est en x */
+int carteY; 
+int pasX;             /* direction vers laquelle on va en x (-1 ou 1) */
+int pasY;
+int touche;
 };
+
+/* Structure de couleur RGB */
+typedef struct Couleur Couleur;
+struct Couleur {
+  int rouge;
+  int vert;
+  int bleu;
+};
+
+
+
+struct Ray{
+  int     
+      type_cote, 
+      hauteur_ligne,  
+      pixel_haut,      
+      pixel_bas;        
+  double cameraX,      
+         rayPosX,     
+	 rayPosY,      
+	 rayDirX,    
+	 rayDirY,      
+         bordDistX,    
+         bordDistY,    
+         deltaDistX,    
+	 deltaDistY,   
+         perpWallDist; 
+     
+};
+
 
 void moteur_dessiner(int x, int largeur, int hauteur);
 void moteur_gestion_des_actions(double temps_frame);
